4dc53cef29c96df8a6145d791a9e56ba50521a48
Sauching <67174563+LeeSauching@users.noreply.github.com>
2025-12-15 13:28:47 +0800

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,9 @@ import os
 import numpy as np
 import torch
 import lgging
+import yaml
+import json
+from types import SimpleNamespace
 
 def seed_everything(seed=42):
     random.seed(seed)
@@ -31,3 +34,18 @@ def init_logger(log_file=f'train.log'):
 IS_KAGGLE_ENV = sum(['KAGGLE' in k for k in os.environ]) > 0
 IS_KAGGLE_SUBMISSION = bool(os.getenv("KAGGLE_IS_COMPETITION_RERUN"))
 suffix = datetime.now().strftime("%Y%m%d%H%M%S")[-1*10:]
+
+def load_config(yaml_path):
+    def _recursive_namespace(d):
+        if isinstance(d, dict):
+            for k, v in d.items():
+                d[k] = _recursive_namespace(v)
+            return SimpleNamespace(**d)
+        elif isinstance(d, list):
+            return [_recursive_namespace(item) for item in d]
+        else:
+            return d
+    with open(yaml_path, 'r', encoding = 'utf-8') as f:
+        cfg_dict = yaml.safe_load(f)
+
+    cfg = _recursive_namespace(cfg_dict)
