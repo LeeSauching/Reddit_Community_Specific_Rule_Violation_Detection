d8406bf159cbdeba1d1a3a70c584429571df7e31
Sauching <67174563+LeeSauching@users.noreply.github.com>
2025-12-21 14:42:53 +0800

    Update model.py

--- src/model.py
+++ src/model.py
@@ -6,7 +6,7 @@ def make_lora_config(CFG) -> LoraConfig:
     return LoraConfig(
         r=CFG['lora_r'],
         lora_alpha=CFG['lora_alpha'],
-        lora_dropout=CFGlora_dropout,
+        lora_dropout=CFG['lora_dropout'],
         bias="none",
         target_modules=["q_proj","k_proj","v_proj","o_proj","gate_proj","up_proj","down_proj"],
         task_type="CAUSAL_LM",
