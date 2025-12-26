f4f06644d9113345b59e9dd1951b491be5024a42
Sauching <67174563+LeeSauching@users.noreply.github.com>
2025-12-26 18:29:58 +0800

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -30,9 +30,8 @@ def init_logger(log_file):
     logger.propagate = False
     return logger
 
-IS_KAGGLE_ENV = sum(['KAGGLE' in k for k in os.environ]) > 0
-IS_KAGGLE_SUBMISSION = bool(os.getenv("KAGGLE_IS_COMPETITION_RERUN"))
-suffix = datetime.now().strftime("%Y%m%d%H%M%S")[-1*10:]
+def is_kaggle_env():
+    return 'KAGGLE_KERNEL_RUN_TYPE' in os.environ
 
 def load_config(yaml_path):
     def _recursive_namespace(d):
@@ -47,6 +46,24 @@ def load_config(yaml_path):
     with open(yaml_path, 'r', encoding = 'utf-8') as f:
         cfg_dict = yaml.safe_load(f)
 
+    on_kaggle = is_kaggle_env()
+
+    if on_kaggle:
+        base_input = "/kaggle/input"
+        base_output = "kaggle/working"
+        
+        if 'input_dir' in cfg_dict:
+            cfg_dict['input_dir'] = os.path.join(base_input, cfg_dict.get('comp_name', ''))
+
+        if 'base_model_path' in cfg_dict:
+            cfg_dict['base_model_path'] = os.path.join(base_input, fg_dict['base_model_path'])
+
+        if 'output_dir' in cfg_dict:
+            cfg_dict['output_dir'] = base_output
+    else:
+        if 'base_model_path' in cfg_dict:
+            cfg_dict['base_model_path'] = os.path.join("./input", cfg_dict['base_model_path'])
+    
     cfg = _recursive_namespace(cfg_dict)
     print(f"Loaded config from {yaml_path}")
     print(json.dumps(cfg_dict, indent=2, ensure_ascii=False))
